ec716659a156d9b3267f865b43d5c95231210158
shigemk2 <i.am.shige@gmail.com>
2016-09-06 00:31:56 +0900

    Add MyActor2 extends Actor

--- src/main/scala/com/example/MyActor2.scala
+++ src/main/scala/com/example/MyActor2.scala
@@ -1,5 +1,18 @@
 package com.example
 
-class MyActor2 {
+import akka.actor.Actor
+import akka.event.Logging
+
+class MyActor2 extends Actor {
+  val log = Logging(context.system, this)
+
+  def receive = {
+    case s: String => {
+      log.info(s)
+      log.info(sender.toString)
+    }
+    case _ => {
+    }
+  }
 
 }
