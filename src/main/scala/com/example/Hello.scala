8e4d5518d0cde7486143d380931d6bce9de1e885
shigemk2 <i.am.shige@gmail.com>
2016-09-08 23:06:12 +0900

    Add child actor and message

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -9,9 +9,14 @@ object Hello {
     // アクターの生成
     val props = Props[MyActor]
     val actor = system.actorOf(props, name = "myActor")
+    // アクターの生成
+    val props2 = Props[MyActor2]
+    val actor2 = system.actorOf(props2, name = "myActor2")
 
     // メッセージを送信
     actor ! "hi"
+    Thread.sleep(1000)
+    actor2 ! "high"
     Thread.sleep(1000)
 
     // 終了
